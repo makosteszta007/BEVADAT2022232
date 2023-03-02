e1be4ca3633a120ba28760f9b14557cb3d1dd4fc
makosteszta <moleve2000@stud.uni-obuda.hu>
2023-03-02 20:42:47 +0100

    first task is done

---
+++ HAZI01/HAZI01.py
@@ -0,0 +1,9 @@
+input_list = [0,1,2,3,4,5,6,7,8,9]
+def subset(start_index,end_index):
+    output=[]
+    for input_list in range(start_index, end_index):
+        output.append(input_list)
+    return output
+
+
+
