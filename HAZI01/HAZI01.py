f38eccada634d60937550efe18065a6692a5ad3e
makosteszta <moleve2000@stud.uni-obuda.hu>
2023-03-02 23:22:51 +0100

    eighth task is done

--- HAZI01/HAZI01.py
+++ HAZI01/HAZI01.py
@@ -11,18 +11,19 @@ def every_nth(input_list):
         output.append(input_list[idx])
     return output
 ##third task
-def unique(input_list):   
-    output = int()
+def unique(input_list):
     for idx in range(0,len(input_list)):
        if input_list[idx]==4:
-        output = input_list[idx]
-    return output==4
+        return True
+    return False
 ##fourth task
 def flatten(input_list):
-    output=[]
-    for idx in range(0,len(input_list)):
-         output.append([input_list[idx]])
-    return output
+    a = len(input_list)
+    for idx in range(0,a):
+         for idx2 in range(0,len(input_list[0])):
+             input_list.append(input_list[0][idx2])
+         input_list.remove(input_list[0])
+    return input_list
 ##fifth task
 def merge_list(*args):
     output=[]
@@ -30,12 +31,19 @@ def merge_list(*args):
           output.append(args)
     return output
 ##sixth task
-def reverse_tuples(input_list):  
-    output=[]
+def reverse_tuples(input_list):      
     for idx in range(0,len(input_list)):       
-        output.append(tuple(reversed(input_list[idx])))
-    return output
+         input_list[idx] = (tuple(reversed(input_list[idx])))
+    return input_list
 ##seventh task
 def remove_duplicates(input_list):
     input_list = list(dict.fromkeys(input_list))
+    return input_list
+##eighth task
+def transpose(input_list):
+    a = len(input_list)
+    for idx in range(0,a):
+         for idx2 in range(0,len(input_list[0])):
+             input_list.append(input_list[0][idx2])
+         input_list.remove(input_list[0])
     return input_list
