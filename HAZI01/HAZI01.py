18d5a255601a66aab588d8c068d40749a881311e
makosteszta <moleve2000@stud.uni-obuda.hu>
2023-03-02 20:52:35 +0100

    second task is done

--- HAZI01/HAZI01.py
+++ HAZI01/HAZI01.py
@@ -1,9 +1,14 @@
+## first task
 input_list = [0,1,2,3,4,5,6,7,8,9]
 def subset(start_index,end_index):
     output=[]
-    for input_list in range(start_index, end_index):
-        output.append(input_list)
+    for idx in range(start_index, end_index):
+        output.append(input_list[idx])
     return output
-
-
+##second task
+def every_nth(n):
+    output=[]
+    for idx in range(0,11,n):
+        output.append(input_list[idx])
+    return output
 
