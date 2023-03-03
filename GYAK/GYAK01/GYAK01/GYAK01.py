945b02bfe12e410bdfcc206b9dddd228b5c450c5
makosteszta <moleve2000@stud.uni-obuda.hu>
2023-03-03 13:50:59 +0100

    first task is done

---
+++ GYAK/GYAK01/GYAK01/GYAK01.py
@@ -0,0 +1,8 @@
+##first task
+def contains_odd(input_list):
+    odd = False
+    for idx in range(0,len(input_list)):
+        if input_list[idx]%2!=0:
+            odd = True
+    return odd
+##second task
