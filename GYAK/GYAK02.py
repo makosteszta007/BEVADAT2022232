308038f503e5f2763d9abd606fd1c6a0554dbd89
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-07 10:52:22 +0100

    second task is done

--- GYAK/GYAK02.py
+++ GYAK/GYAK02.py
@@ -1,6 +1,10 @@
 import numpy as np
 ##first task
-def create_array(size=(2,2)):
+def create_array(size=(2,2))->np.array:
     return np.zeros(size)
-    
-
+##second task
+def set_one(input_array):  
+    input_array=np.array(input_array)
+    np.fill_diagonal(input_array,1)   
+    return input_array
+print(set_one([[0,2],[2,3]]))
