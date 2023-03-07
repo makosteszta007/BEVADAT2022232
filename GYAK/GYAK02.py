f73dd04a04021e63e001597c6471008d6b307efe
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-07 10:59:35 +0100

    third task is done

--- GYAK/GYAK02.py
+++ GYAK/GYAK02.py
@@ -7,4 +7,8 @@ def set_one(input_array):
     input_array=np.array(input_array)
     np.fill_diagonal(input_array,1)   
     return input_array
-print(set_one([[0,2],[2,3]]))
+##third task
+def do_transpose(input_matrix):
+    np.transpose(input_matrix)
+    return input_matrix
+
