cc294d306de64d32c0a04e5f14c4497a33050b52
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-07 11:17:08 +0100

    GYAK02 is done

--- GYAK/GYAK02.py
+++ GYAK/GYAK02.py
@@ -3,24 +3,23 @@ import numpy as np
 def create_array(size=(2,2))->np.array:
     return np.zeros(size)
 ##second task
-def set_one(input_array):  
+def set_one(input_array)->np.array:  
     input_array=np.array(input_array)
     np.fill_diagonal(input_array,1)   
     return input_array
 ##third task
-def do_transpose(input_matrix):
+def do_transpose(input_matrix)->np.array:
     input_matrix=np.transpose(input_matrix)
     return input_matrix
 ##fourth task
-def round_array(input_array):        
+def round_array(input_array)->np.array:        
     return np.array(input_array).round(2)
 ##fifth task
-def bool_array(input_array):
+def bool_array(input_array)->np.array:
     return np.array(input_array,dtype=bool)
 ##sixth task
-def invert_bool_array(input_array):
+def invert_bool_array(input_array)->np.array:
     return np.invert(np.array(input_array,dtype=bool))
 ##seventh task
-def flatten(input_array):    
+def flatten(input_array)->np.array:    
     return np.ndarray.flatten(np.array(input_array))
-print(flatten([[1,2],[3,4]]))
