0316c76b6ed762785167c8cf93dd713e796ac566
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-26 20:52:19 +0200

    tenth task is done

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -5,28 +5,24 @@ import numpy as np
 def csv_to_df(df='HAZI\HAZI04\StudentsPerformance.csv'):
     df_data= pn.read_csv(df, sep=',')
     return df_data
-print(csv_to_df())
 ##second task
 def capitalize_columns(df):
     df = pn.DataFrame(df)
     df_data_capitalized = df.copy()
     df_data_capitalized.columns = [col.capitalize() if 'e' in col else col for col in df.columns]
     return df_data_capitalized
-print(capitalize_columns(csv_to_df()))
 ##third task
 def math_passed_count(df) -> int:
     df = pn.DataFrame(df)
     new_df = df.copy()
     passed = (new_df['math score']>=50).sum()
     return passed
-print(math_passed_count(csv_to_df()))
 ##fourth task
 def did_pre_course(df):
     df = pn.DataFrame(df)
     new_df = df.copy()
     df_did_precourse=new_df[new_df['test preparation course'].values == "completed"]
     return df_did_precourse
-print(did_pre_course(csv_to_df()))
 ##fifth task
 def avarage_scores(df):
     df = pn.DataFrame(df)
@@ -34,7 +30,6 @@ def avarage_scores(df):
     groupby = df.groupby('parental level of education')
     avarage_scores = groupby['math score', 'reading score', 'writing score'].mean()
     return avarage_scores
-print(avarage_scores(csv_to_df()))
 ##sixth task
 def add_age(df):
     df = pn.DataFrame(df)
@@ -43,7 +38,6 @@ def add_age(df):
     ages = np.random.randint(18, 67, size=len(new_df))
     new_df['age'] = ages
     return new_df
-print(add_age(csv_to_df()))
 ##seventh task 
 def female_top_score(df):
     df = pn.DataFrame(df)
@@ -51,7 +45,6 @@ def female_top_score(df):
     female_df= new_df[new_df['gender']=='female']
     tuple_max = [female_df['math score'].max(),female_df['reading score'].max(),female_df['writing score'].max()]
     return tuple_max
-print(female_top_score(csv_to_df()))
 ##eighth task
 def add_grade(df):
     df = pn.DataFrame(df)
@@ -70,7 +63,6 @@ def add_grade(df):
             return 'F'
     new_df['grade'] = new_df['percentage'].apply(get_grade)
     return new_df
-print(add_grade(csv_to_df()))
 ##ninth task
 def math_bar_plot(df):
     df = pn.DataFrame(df)
@@ -82,4 +74,13 @@ def math_bar_plot(df):
     grouped_data = new_df.groupby('gender').mean(numeric_only=True)
     ax.bar(grouped_data.index, grouped_data['math score']) 
     return fig
-print(math_bar_plot(csv_to_df()))
+##tenth task
+def writing_hist(df):
+    df = pn.DataFrame(df)
+    new_df = df.copy()
+    fig, ax = plt.subplots()
+    ax.hist(new_df['writing score'], bins=20)
+    ax.set_title('Distribution of Writing Scores')
+    ax.set_xlabel('Writing Score')
+    ax.set_ylabel('Number of Students')
+    return fig
