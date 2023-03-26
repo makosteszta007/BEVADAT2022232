ced6cceeaaeae61d4057e229d04bd9a91599213a
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-26 19:39:56 +0200

    second task is done

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -2,6 +2,14 @@ import pandas as pn
 import matplotlib.pyplot as plt
 import numpy as np
 ##first task
-def dict_to_dataframe(test_dict='HAZI\HAZI04\StudentsPerformance.csv'):
-    test_df = pn.read_csv(test_dict, sep=",")
-    return test_df
+def csv_to_df(df='HAZI\HAZI04\StudentsPerformance.csv'):
+    df_data= pn.read_csv(df, sep=',')
+    return df_data
+print(csv_to_df())
+##second task
+def capitalize_columns(df):
+    df = pn.DataFrame(df)
+    df_data_capitalized = df.copy()
+    df_data_capitalized.columns = [col.capitalize() if 'e' in col else col for col in df.columns]
+    return df_data_capitalized
+print(capitalize_columns(csv_to_df()))
