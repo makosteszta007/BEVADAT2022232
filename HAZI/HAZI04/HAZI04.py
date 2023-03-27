74a8cdb8bd02c92522d6d964a67b510da96bef42
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-03-27 20:06:42 +0200

    pn renamed pd

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -1,46 +1,46 @@
-import pandas as pn
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 ##first task
 def csv_to_df(df='HAZI\HAZI04\StudentsPerformance.csv'):
-    df_data= pn.read_csv(df, sep=',')
+    df_data= pd.read_csv(df, sep=',')
     return df_data
 ##second task
-def capitalize_columns(df: pn.DataFrame) -> pn.DataFrame:   
+def capitalize_columns(df: pd.DataFrame) -> pd.DataFrame:   
     df_data_capitalized = df.copy()
     df_data_capitalized.columns = [col.capitalize() if 'e' not in col else col for col in df.columns]
     return df_data_capitalized
 ##third task
-def math_passed_count(df: pn.DataFrame) -> int:
+def math_passed_count(df: pd.DataFrame) -> int:
     new_df = df.copy()    
     passed = (new_df['math score']>=50).sum()
     return passed
 ##fourth task
-def did_pre_course(df: pn.DataFrame):    
+def did_pre_course(df: pd.DataFrame):    
     new_df = df.copy()
     df_did_precourse=new_df[new_df['test preparation course'].values == "completed"]
     return df_did_precourse
 ##fifth task
-def avarage_scores(df: pn.DataFrame) -> pn.DataFrame:   
+def avarage_scores(df: pd.DataFrame) -> pd.DataFrame:   
     new_df = df.copy()
     groupby = new_df.groupby('parental level of education')
     avarage_scores = groupby[['math score', 'reading score', 'writing score']].mean()
     return avarage_scores
 ##sixth task
-def add_age(df: pn.DataFrame):   
+def add_age(df: pd.DataFrame):   
     new_df = df.copy()
     np.random.seed(42)
     ages = np.random.randint(18, 67, size=len(new_df))
     new_df['age'] = ages
     return new_df
 ##seventh task 
-def female_top_score(df: pn.DataFrame) -> tuple:    
+def female_top_score(df: pd.DataFrame) -> tuple:    
     new_df = df.copy()
     female_df= new_df[new_df['gender']=='female']
     tuple_max = (female_df['math score'].max(),female_df['reading score'].max(),female_df['writing score'].max())
     return tuple_max
 ##eighth task
-def add_grade(df: pn.DataFrame) -> pn.DataFrame:   
+def add_grade(df: pd.DataFrame) -> pd.DataFrame:   
     new_df = df.copy()        
     def get_grade(percentage):
         if percentage >= 0.9:
@@ -57,7 +57,7 @@ def add_grade(df: pn.DataFrame) -> pn.DataFrame:
     return new_df
 ##ninth task
 def math_bar_plot(df):
-    df = pn.DataFrame(df)
+    df = pd.DataFrame(df)
     new_df = df.copy()
     fig, ax = plt.subplots()
     ax.set_title('Average Math Score by Gender')
@@ -67,7 +67,7 @@ def math_bar_plot(df):
     ax.bar(grouped_data.index, grouped_data['math score']) 
     return fig
 ##tenth task
-def writing_hist(df: pn.DataFrame):   
+def writing_hist(df: pd.DataFrame):   
     new_df = df.copy()
     fig, ax = plt.subplots(figsize=(8,6))
     ax.hist(new_df['writing score'], bins=20, color='#5E86C1')
