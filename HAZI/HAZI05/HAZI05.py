ba123c65d8a1df053ebae8d99eb127e79bbf139d
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-04-04 23:39:31 +0200

    HAZI05 again

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -5,56 +5,64 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 class KNNClassifier:
- 
-##first task
- def k_neighbors(self) -> int:
-    return self.k
-##second task
-def __init__(self, k:int, test_split_ratio: float):
-     self.k = k
-     self.test_split_ratio = test_split_ratio
-##third task
-@staticmethod
-def load_csv(path: str) -> Tuple[pd.core.frame.DataFrame, pd.core.series.Series]:
-     dataFrame = pd.read_csv(path)
-     dataFrame = dataFrame.sample(frac=1, random_state=42).reset_index(drop=True)
-     return dataFrame.iloc[:,:-1], dataFrame.iloc[:,-1]
-##fourth task  
-def train_test_split(self, features:pd.core.frame.DataFrame,labels:pd.core.series.Series):    
-     test_size = int(len(features) * self.test_split_ratio)
-     train_size = len(features) - test_size
-     assert len(features) == test_size + train_size, "Size mismatch!"
+    @property
+    def k_neighbors(self) -> int:
+        return self.k
 
-     self.x_train,self.y_train = features.iloc[:train_size,:],labels.iloc[:train_size]
-     self.x_test,self.y_test = features.iloc[train_size:train_size+test_size,:], labels.iloc[train_size:train_size + test_size]
-##fifth task
-def euclidean(self, element_of_x:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
-     return ((self.x_train - element_of_x)**2).sum(axis=1) ** .5
-##sixth task
-def predict(self, x_test: pd.core.frame.DataFrame):
-     labels_pred = []
-     for index, x_test_element in x_test.iterrows():
-         distances = self.euclidean(x_test_element)
-         distances = pd.DataFrame(sorted(zip(distances, self.y_train)))
-         label_pred = distances.iloc[:self.k,1].mode()
-         labels_pred.append(label_pred)
+    def __init__(self, k:int, test_split_ratio: float):
+        self.k = k
+        self.test_split_ratio = test_split_ratio
 
-     self.y_preds = pd.DataFrame(labels_pred).iloc[:,0]
-##seventh task
-def accuracy(self) -> float:
+    @staticmethod
+    def load_csv(path: str) -> Tuple[pd.core.frame.DataFrame, pd.core.series.Series]:
+        dataFrame = pd.read_csv(path)
+        dataFrame = dataFrame.sample(frac=1, random_state=42).reset_index(drop=True)
+        return dataFrame.iloc[:,:-1], dataFrame.iloc[:,-1]
+    
+    def train_test_split(self,
+                         features:pd.core.frame.DataFrame,
+                         labels:pd.core.series.Series):
+    
+        test_size = int(len(features) * self.test_split_ratio)
+        train_size = len(features) - test_size
+        assert len(features) == test_size + train_size, "Size mismatch!"
+
+        self.x_train,self.y_train = features.iloc[:train_size,:],labels.iloc[:train_size]
+        self.x_test,self.y_test = features.iloc[train_size:train_size+test_size,:], labels.iloc[train_size:train_size + test_size]
+
+    def euclidean(self, element_of_x:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+        return ((self.x_train - element_of_x)**2).sum(axis=1) ** .5
+    
+    def predict(self, x_test: pd.core.frame.DataFrame):
+        labels_pred = []
+        for index, x_test_element in x_test.iterrows():
+            distances = self.euclidean(x_test_element)
+            distances = pd.DataFrame(sorted(zip(distances, self.y_train)))
+            label_pred = distances.iloc[:self.k,1].mode()
+            labels_pred.append(label_pred)
+
+        self.y_preds = pd.DataFrame(labels_pred).iloc[:,0]
+
+    def accuracy(self) -> float:
         true_positive = (self.y_test.reset_index(drop=True) == self.y_preds.reset_index(drop=True)).sum()
         return true_positive/len(self.y_test) * 100
-##eight task    
-def confusion_matrix(self):
+    
+    def confusion_matrix(self):
         conf_matrix = confusion_matrix(self.y_test, self.y_preds)
         return conf_matrix
-##ninth task
-def best_k(self) -> Tuple[int, float]:
-        accuracies = []
-        for i in range(1, 21):
-            self.k = i
+    
+    def best_accuracy(self) -> Tuple[int, float]:
+        best_k = 0
+        best_pred_accuracy = 0.0
+        temp_k = self.k
+        for i in range(10):
+            self.k = i*2+1
             self.predict(self.x_test)
-            acc = self.accuracy()
-            accuracies.append((i, acc))
-        best_k, best_acc = max(accuracies, key=lambda x: x[1])
-        return (best_k, round(best_acc, 2))
+            pred_accuracy = self.accuracy()
+            if (best_pred_accuracy < pred_accuracy):
+                best_k = self.k
+                best_pred_accuracy = pred_accuracy
+        
+        self.k = temp_k
+        
+        return best_k, round(best_pred_accuracy, 2)
